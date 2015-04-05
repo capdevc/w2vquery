b32680df8a68b063a2e3759a75b718ae32865c3f
Cristian Capdevila <ccapdevi@utk.edu>
2015-04-05 13:21:01 -0400

    final cleanup

--- src/main/scala/com/pyaanalytics/W2VQuery.scala
+++ src/main/scala/com/pyaanalytics/W2VQuery.scala
@@ -17,30 +17,15 @@
 package com.pyaanalytics
 
 import org.apache.spark.mllib.feature._
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import org.apache.spark.rdd.RDD
 
 import scopt.OptionParser
 import java.io.{ObjectInputStream, FileInputStream}
-import scala.io.Source
-import scala.xml._
 
 object W2VQuery {
 
   case class W2VQueryConfig(queryString: String = "",
                             modelFile: String = "",
                             sparkMaster: String = "local[64]")
-
-  class ModelLoader(
-    fileInputStream: FileInputStream
-  ) extends ObjectInputStream(fileInputStream) {
-    override def resolveClass(desc: java.io.ObjectStreamClass): Class[_] = {
-      try { Class.forName(desc.getName, false, getClass.getClassLoader) }
-      catch { case ex: ClassNotFoundException => super.resolveClass(desc) }
-    }
-  }
 
   def main(args: Array[String]): Unit = {
 
@@ -70,21 +55,12 @@ object W2VQuery {
   }
 
   def run(config: W2VQueryConfig): Unit = {
-    // val sparkConf = new SparkConf()
-    //   .setAppName("W2VQuery")
-    //   .setMaster(config.sparkMaster)
-    //   .set("spark.executor.memory", "10g")
-
-    // val sc = new SparkContext(sparkConf)
 
     val iStream = new ObjectInputStream(new FileInputStream(config.modelFile))
-    val modelLoader = new ModelLoader(new FileInputStream(config.modelFile))
     val w2vModel = iStream.readObject().asInstanceOf[Word2VecModel]
 
     val synonyms = w2vModel.findSynonyms(config.queryString, 20)
 
     synonyms map {x => println(x.toString)}
-
-    // sc.stop()
   }
 }
